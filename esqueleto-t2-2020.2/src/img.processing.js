93f85b96f5f3c72af001c2a960d73fbf5592daf9
Gabriel Aureo <gabrielaureo@gmail.com>
2021-06-05 19:51:29 -0300

    removed debug logs and added 3x3 inverse matrix calculation

--- esqueleto-t2-2020.2/src/img.processing.js
+++ esqueleto-t2-2020.2/src/img.processing.js
@@ -10,6 +10,8 @@
         box : [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
         laplace : [[0, -1, 0], [-1, 4, -1], [0, -1, 0]]
     }
+
+    const CANVAS_SIZE = [600, 600];
     
     nj.config.printThreshold = 28
     function ImageProcesser(img, kernel = null, xform = null, bhandler = 'icrop') {
@@ -40,14 +42,10 @@
         }
 
         let new_matrix = nj.zeros(matrix.shape)
-        console.log(matrix.toString())
         for(let i = 1 ; i < matrix.shape[0] - 1; i++){
             for(let j = 1; j < matrix.shape[1] - 1; j++){
                 
                 let sub = matrix.slice([i - 1, i + 2], [j - 1, j + 2])
-                if (j == 1){
-                    console.log(sub.toString())
-                }
                 let f = multiply(sub, nj.array(kernel).T).sum()
 
                 new_matrix.set(i,j, f)
@@ -60,6 +58,51 @@
         return new_matrix
     }
 
+    function inverse_matrix(matrix){
+    //Baseado em: https://byjus.com/maths/inverse-of-3-by-3-matrix/
+        var determinant = det_3x3(matrix);
+        var cofactor = cofactor_3x3(matrix).T;
+        var adjugate = cofactor.T;
+        var coeff = 1/determinant;
+        var inversed = adjugate.multiply(coeff);
+
+        return inversed;
+    }
+    function det_3x3(matrix){
+        var A = nj.array([[matrix.get(1,1), matrix.get(1,2)],[matrix.get(2,1), matrix.get(2,2)]]);
+        var B = nj.array([[matrix.get(1,0), matrix.get(1,2)],[matrix.get(1,2), matrix.get(2,2)]])
+        var C = nj.array([[matrix.get(1,0), matrix.get(1,1)],[matrix.get(2,0), matrix.get(2,1)]])
+
+        return matrix.get(0,0)*det_2x2(A) - matrix.get(0,1)*det_2x2(B) - matrix.get(0,2)*det_2x2(C);
+    }
+    function det_2x2(matrix){
+        return matrix.get(0,0)*matrix.get(1,1) - matrix.get(0,1)*matrix.get(1,0);
+    }
+    function cofactor_3x3(matrix){
+        return nj.array([[ matrix.get(0,0), -matrix.get(0,1),  matrix.get(0,2)],
+                         [-matrix.get(1,0),  matrix.get(1,1), -matrix.get(1,2)],
+                         [ matrix.get(2,0), -matrix.get(2,1),  matrix.get(2,2)]]);
+    }
+
+    function bilinear_filtering(image, x, y){
+
+        let i = Math.floor( x + 0.5) - 0.5
+        let j = Math.floor( y + 0.5) - 0.5
+
+        let a = x - i;
+        let b = y - j;
+        let color = 255
+
+        if (i > 0 && i < image.shape[0] && j > 0 && j < image.shape[1]){
+            color = (1-a)*(1-b) * image.get(i-0.5, j-0.5) + 
+            a*(1-b) * image.get(i+0.5, j-0.5) +
+            a*b * image.get(i+0.5, j+0.5) +
+            (1-a)*b * image.get(i-0.5, j+0.5);
+        }
+
+        return color;
+    }
+
     function extend(matrix){
         let data = matrix.clone()
         //Expand bordas horizontais
@@ -70,17 +113,6 @@
         data = nj.concatenate(data.slice([0,1], null).T, data.T).T
         return data;
     }
-    function dot(m1, m2){
-        let ret = nj.zeros([m1.shape[0], m2.shape[1]])
-        for(let i = 0; i < m1.shape[0]; i++){
-            for(let j = 0; j < m1.shape[1]; j++){
-                let sum = 0
-                
-                sum += m1.get(i,j) * m2.get(i,j)
-            }
-        }
-    }
-
     
 
     Object.assign( ImageProcesser.prototype, {
@@ -121,20 +153,27 @@
 
         },
         
+        
 
         apply_xform: function()  {
-            // let data = nj.ones(this.img.shape)
-            // console.log(data.shape)
-            // for(let i = 0; i < data.shape[0]; i++){
-            //     for(let j = 0; j < data.shape[1]; j++){
-            //         //console.log([i,j])
-            //         let new_pos = nj.dot(this.xform, nj.array([i,j,1]).T)
-            //         data.set(new_pos[0], new_pos[1], this.img.get(i,j))
-            //     }
-            // }
-            // this.img = data
-        },
 
+            let out = nj.ones(CANVAS_SIZE).multiply(255)
+            let inv = inverse_matrix(this.xform)
+
+            for(let l = - CANVAS_SIZE[0]/2; l <  CANVAS_SIZE[0]/2; l ++){
+                for(let c = - CANVAS_SIZE[1]/2; c <  CANVAS_SIZE[1]/2; c ++){
+                    var out_coord = nj.array([l + .5, c +.5, 1])
+                    let in_coord = nj.dot(inv, out_coord.T)
+
+                    let color = bilinear_filtering(this.img, in_coord.get(0), in_coord.get(1));
+                    out.set(l+CANVAS_SIZE[0]/2 , c+CANVAS_SIZE[1]/2, color);
+                }
+            }
+            this.img = out
+            this.width = CANVAS_SIZE[0]
+            this.height = CANVAS_SIZE[1]
+            
+        },
         update: function() {
             // Method to process image and present results
             var start = new Date().valueOf();
